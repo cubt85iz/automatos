2ead5d63f93ae78b91400dd1ab1ad9cbe373b8f4
P H <git@cubt85iz>
2025-04-23 20:38:57 -0400

    Clean up install.sh and switch to raw.

--- install.sh
+++ install.sh
@@ -13,18 +13,18 @@ install_github_releases() {
 
 install_packages() {
   PACKAGES=$(jq -r '.packages.install | join(" ")' config.json)
-  rpm-ostree install $PACKAGES
+  rpm-ostree install "$PACKAGES"
 }
 
 remove_packages() {
   PACKAGES=$(jq -r '.packages.remove | join(" ")' config.json)
-  rpm-ostree override remove $PACKAGES
+  rpm-ostree override remove "$PACKAGES"
 }
 
 install_repos() {
-  for REPO in $(jq -c ".repos[]" config.json); do
+  for REPO in $(jq -r ".repos[]" config.json); do
     REPO_NAME=${REPO##*/}
-    curl -L $REPO -o /etc/yum.repos.d/$REPO_NAME
+    curl -L "$REPO" -o "/etc/yum.repos.d/$REPO_NAME"
   done
 }
 
