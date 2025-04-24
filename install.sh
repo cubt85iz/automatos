d80943d59c48b81e513c213b161fcb48254ce7d7
P H <git@cubt85iz>
2025-04-24 17:43:30 -0400

    Attempt to override free packages.

--- install.sh
+++ install.sh
@@ -16,6 +16,16 @@ install_packages() {
   rpm-ostree install $PACKAGES
 }
 
+override_packages() {
+  true
+  OVERRIDES=$(jq -r '.packages.overrides[] | .legacy + ":" + .new' config.json)
+  for OVERRIDE in ${OVERRIDES[@]}; do
+    LEGACY=${OVERRIDE%%*:}
+    NEW=${OVERRIDE#:*}
+    rpm-ostree override remove $LEGACY --install $NEW
+  done
+}
+
 remove_packages() {
   PACKAGES=$(jq -r '.packages.remove | join(" ")' config.json)
   rpm-ostree override remove $PACKAGES
@@ -24,16 +34,24 @@ remove_packages() {
 install_repos() {
   for REPO in $(jq -r ".repos[]" config.json); do
     REPO_NAME=${REPO##*/}
-    curl -L "$REPO" -o "/etc/yum.repos.d/$REPO_NAME"
+    REPO_TYPE=${REPO_NAME##*.}
+    if [ "$REPO_TYPE" == "rpm" ]; then
+      rpm-ostree install $REPO
+    else
+      curl -L "$REPO" -o "/etc/yum.repos.d/$REPO_NAME"
+    fi
   done
 }
 
 setup() {
   # Install third-party repositories.
   install_repos
-  
+
   # Remove undesired packages
   remove_packages
+
+  # Apply package overrides
+  override_packages
 
   # Install desired appimages, flatpaks, & packages
   install_github_releases
