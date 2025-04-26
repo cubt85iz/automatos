ddccf91b8ece3bbe0bb4c504c8d280db72d40f02
P H <git@cubt85iz>
2025-04-26 08:05:36 -0400

    Refactors replacement and installation of packages.

--- install.sh
+++ install.sh
@@ -12,24 +12,35 @@ install_github_releases() {
 }
 
 install_packages() {
-  PACKAGES=$(jq -r '.packages.install | join(" ")' config.json)
-  rpm-ostree install $PACKAGES
-}
+  # Get list of packages for installation.
+  readarray -t PKGLIST < <(jq -c '.packages.install[]' config.json)
 
-override_packages() {
-  readarray -t OVERRIDES < <(jq -r '.packages.overrides[] | .legacy + ":" + .new' config.json)
-  if [ ${#OVERRIDES[@]} -gt 0 ]; then
-    for OVERRIDE in "${OVERRIDES[@]}"; do
-      read -a LEGACY <<< "${OVERRIDE%:*}"
-      NEW=${OVERRIDE##*:}
-      rpm-ostree override remove ${LEGACY[@]} --install "$NEW"
-    done
-  fi
+  # Install packages.
+  rpm-ostree install "${PKGLIST[@]}"
 }
 
 remove_packages() {
-  PACKAGES=$(jq -r '.packages.remove | join(" ")' config.json)
-  rpm-ostree override remove $PACKAGES
+  # Get list of objects containing packages for removal/replacement.
+  readarray -t PKGLIST < <(jq -c '.packages.remove[]' config.json)
+
+  if [ ${#PKGLIST[@]} -gt 0 ]; then
+    for PKG in "${PKGLIST[@]}"; do
+      # Get list of packages to remove from object
+      readarray -t REMOVE < <(echo "$PKG" | jq -r '.remove[]')
+
+      # Get replacement package
+      REPLACE=$(echo "$PKG" | jq -r '.replace[]')
+
+      # Replace package(s)
+      if [ -n "$REPLACE" ]; then
+        rpm-ostree override remove "${REMOVE[@]}" --install "$REPLACE"
+
+      # Remove package(s)
+      else
+        rpm-ostree override remove "${REMOVE[@]}"
+      fi
+    done
+  fi
 }
 
 install_repos() {
@@ -50,9 +61,6 @@ setup() {
 
   # Remove undesired packages
   remove_packages
-
-  # Apply package overrides
-  override_packages
 
   # Install desired appimages, flatpaks, & packages
   install_github_releases
